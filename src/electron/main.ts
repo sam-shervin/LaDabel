731e5e922a513b02c487b7c49d7201381ce656e5
sam-shervin <samshervin.sebastian@gmail.com>
2025-10-21 15:04:12 +0530

    feat: update main.ts to load URL in development mode
    - Added a check to determine if the application is running in development mode using the isDev() function.
    - If in development mode, the application now loads the URL 'http://localhost:5123'.
    - In production mode, the application continues to load the index.html file from the dist-ui directory.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,13 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import {isDev} from './util.js';
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-ui/index.html'));
+    if (isDev()) {
+        mainWindow.loadURL('http://localhost:5123');
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-ui/index.html'));
+    }
 });
 
